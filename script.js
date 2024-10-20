f31299afe2bc731e814ad65655da89f32b62d294
iglesiasiniestra <pastorsiniestro@iglesiasiniestra.org>
2024-10-20 00:14:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
 hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');  // Toggles the visibility of the nav-links
+    navLinks.classList.toggle('active');  // Toggle the active class to show/hide nav-links
 });
